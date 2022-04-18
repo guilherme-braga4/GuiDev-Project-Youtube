fe1f8742eba9ca1a6fe79987dad834f7fd7e7701
Guilherme Braga <bragaguilherme152@yahoo.com.br>
2022-04-18 20:56:05 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,49 +1,77 @@
-import './App.css'
-import React, { useState } from 'react'
-import InputMask from 'react-input-mask'
-import { findByPlaceholderText } from '@testing-library/react'
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import InputMask from "react-input-mask";
 
 function App() {
-  const [form, setForm] = useState('')
+  //Aqui iremos tornar a nossa "página dinâmica", através do Javascript
+  //Estado para armazenar Altura e Peso
+  const [form, setForm] = useState("");
 
+  //Função para armazenar o que foi digitado pelo usuário, dentro dos Inputs
+  //Iremos criar um objeto
   function handleChange(event) {
-    console.log('event 1', event.target.name)
-    console.log('event 2', event.target.value)
-    setForm({ ...form, [event.target.name]: event.target.value })
-    console.log('form', form)
+    console.log("event 1", event.target.name);
+    console.log("event 2", event.target.value);
+    setForm({ ...form, [event.target.name]: event.target.value });
+    console.log("form", form);
   }
 
+  //Função para enviar para a API (Usaremos o console para ver o Network)
   function handleSubmit(event) {
-    event.preventDefault()
-    if ((!form.altura && !form.peso) || !form.peso || !form.altura) {
-      alert('Os valores não podem ser inválidos. Digite o valor corretamente')
+    event.preventDefault();
+    console.log("estamos dentro do handleSubmit");
+    console.log("form handleSubmit", form);
+    if (form.altura !== "" && form.peso !== "") {
+      const imc = form.peso / (form.altura * form.altura);
+      console.log("imc", imc);
+      alert("Seu IMC é " + imc);
     } else {
-      const imc = form.peso / (form.altura * form.altura)
-      console.log('imc', imc)
-      alert('O seu IMC é ' + imc + ' e sua classificação é Magreza')
+      alert("Os valores precisam ser maiores do que 0");
     }
+    // Desafio: implemente uma lógica para verificar o IMC, e junto com o Alert, mostrar a classificação do IMC
+    // Por exemplo: "Seu IMC é 30 e sua Classificação é Sobrepeso"
+    // if ("alguma coisa") {
+    //   "isso acontece"
+    // }
+    // else {
+    //   "outra coisa acontece"
+    // }
+
+    // // ou você pode precisar por mais de 2 condições, e usar o Else If
+    // if ("alguma coisa") {
+    //   "isso acontece"
+    // }
+    // else if ("outra coisa") {
+    //  "outra coisa acontece"
+    // }
+    // else {
+    //   "por fim isso acontece"
+    // }
   }
 
+  //JSX, equivale ao nosso HTML e CSS
   return (
     <form onSubmit={handleSubmit}>
       <div className="container">
         <div className="form">
           <div className="container-input-label">
-            <div className="children-container-input-label">
+            <div className="childen-container-input-label">
               <label>Quanto você mede?</label>
               <InputMask
-                placeholder="Digite a sua Altura"
-                mask="9.99"
+                placeholder="ALTURA"
                 onChange={handleChange}
                 name="altura"
+                mask="9.99"
               />
             </div>
-            <div className="children-container-input-label">
+            <div className="childen-container-input-label">
               <label>Quanto você pesa?</label>
               <InputMask
-                placeholder="Digite o seu Peso"
+                placeholder="PESO"
                 onChange={handleChange}
                 name="peso"
+                // type="number"
+                // mask=""
               />
             </div>
           </div>
@@ -53,7 +81,7 @@ function App() {
         </div>
       </div>
     </form>
-  )
+  );
 }
 
-export default App
+export default App;
