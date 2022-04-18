34b82bdfa8c089d2adba389f8318990e9dc90c17
Guilherme Braga <bragaguilherme152@yahoo.com.br>
2022-04-18 18:18:10 -0300

    project_done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, { useState } from 'react'
+import InputMask from 'react-input-mask'
+import { findByPlaceholderText } from '@testing-library/react'
 
 function App() {
+  const [form, setForm] = useState('')
+
+  function handleChange(event) {
+    console.log('event 1', event.target.name)
+    console.log('event 2', event.target.value)
+    setForm({ ...form, [event.target.name]: event.target.value })
+    console.log('form', form)
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    if ((!form.altura && !form.peso) || !form.peso || !form.altura) {
+      alert('Os valores não podem ser inválidos. Digite o valor corretamente')
+    } else {
+      const imc = form.peso / (form.altura * form.altura)
+      console.log('imc', imc)
+      alert('O seu IMC é ' + imc + ' e sua classificação é Magreza')
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <form onSubmit={handleSubmit}>
+      <div className="container">
+        <div className="form">
+          <div className="container-input-label">
+            <div className="children-container-input-label">
+              <label>Quanto você mede?</label>
+              <InputMask
+                placeholder="Digite a sua Altura"
+                mask="9.99"
+                onChange={handleChange}
+                name="altura"
+              />
+            </div>
+            <div className="children-container-input-label">
+              <label>Quanto você pesa?</label>
+              <InputMask
+                placeholder="Digite o seu Peso"
+                onChange={handleChange}
+                name="peso"
+              />
+            </div>
+          </div>
+          <div className="container-button">
+            <button>CALCULAR</button>
+          </div>
+        </div>
+      </div>
+    </form>
+  )
 }
 
-export default App;
+export default App
