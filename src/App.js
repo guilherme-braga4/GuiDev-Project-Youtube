4fb6974775a6b9689587d41e184c4445766ffeca
Guilherme Braga <bragaguilherme152@yahoo.com.br>
2022-04-18 21:00:27 -0300

    fix: css and validation

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import "./App.css";
-import React, { useState, useEffect } from "react";
-import InputMask from "react-input-mask";
+import './App.css'
+import React, { useState, useEffect } from 'react'
+import InputMask from 'react-input-mask'
 
 function App() {
   //Aqui iremos tornar a nossa "página dinâmica", através do Javascript
   //Estado para armazenar Altura e Peso
-  const [form, setForm] = useState("");
+  const [form, setForm] = useState('')
 
   //Função para armazenar o que foi digitado pelo usuário, dentro dos Inputs
   //Iremos criar um objeto
   function handleChange(event) {
-    console.log("event 1", event.target.name);
-    console.log("event 2", event.target.value);
-    setForm({ ...form, [event.target.name]: event.target.value });
-    console.log("form", form);
+    console.log('event 1', event.target.name)
+    console.log('event 2', event.target.value)
+    setForm({ ...form, [event.target.name]: event.target.value })
+    console.log('form', form)
   }
 
   //Função para enviar para a API (Usaremos o console para ver o Network)
   function handleSubmit(event) {
-    event.preventDefault();
-    console.log("estamos dentro do handleSubmit");
-    console.log("form handleSubmit", form);
-    if (form.altura !== "" && form.peso !== "") {
-      const imc = form.peso / (form.altura * form.altura);
-      console.log("imc", imc);
-      alert("Seu IMC é " + imc);
+    event.preventDefault()
+    console.log('estamos dentro do handleSubmit')
+    console.log('form handleSubmit', form)
+    if ((!form.altura && !form.peso) || !form.peso || !form.altura) {
+      const imc = form.peso / (form.altura * form.altura)
+      console.log('imc', imc)
+      alert('Seu IMC é ' + imc)
     } else {
-      alert("Os valores precisam ser maiores do que 0");
+      alert('Os valores precisam ser maiores do que 0')
     }
     // Desafio: implemente uma lógica para verificar o IMC, e junto com o Alert, mostrar a classificação do IMC
     // Por exemplo: "Seu IMC é 30 e sua Classificação é Sobrepeso"
@@ -81,7 +81,7 @@ function App() {
         </div>
       </div>
     </form>
-  );
+  )
 }
 
-export default App;
+export default App
